01d2799dd8c9d296c19e23922fb0ee7ca09a3057
Hien Nguyen <dachienit@gmail.com>
2025-11-27 23:34:46 +0700

    fix: update index.js

--- console/bin/index.js
+++ console/bin/index.js
@@ -14,7 +14,7 @@ import fs from 'node:fs';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename)
 
-// test
+// test 1
 
 dotenv.config({path: `${__dirname}/../.env`})
 
